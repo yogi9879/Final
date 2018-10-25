a1ce64c2b283fe97feb5fa99e22dbea9962088ca
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-26 02:06:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def hello_world():
   return 'Hey its Python Flask application!'
 
 @app.route('/hello')
-def hello_world():
+def hello_world1():
   return 'Hey its Python application!'
 
 if __name__ == '__main__':
