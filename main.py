268da3cf94f1d3195e0701446bc49da23c2fb3c4
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-26 12:21:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ def hello_world1():
     
         f = request.files['file']
         
-       # f.save(secure_filename(f.filename))
-        f.save(os.path.join(app.config['/static'], secure_filename(f.filename)))
+        f.save(secure_filename(f.filename))
+       
         return 'file uploaded successfully'
 
 if __name__ == '__main__':
