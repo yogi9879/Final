8095ae680d46d809cfcc926241708374b1b1baa0
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-26 11:27:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,14 +8,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
   return  render_template("frontpage.html")
-'''
+
 @app.route('/hello', methods = ['GET', 'POST'])
 def hello_world1():
   f= = request.files['file']
   
   return  render_template("frontpage.html")
 
-'''
 
 if __name__ == '__main__':
   app.run()
