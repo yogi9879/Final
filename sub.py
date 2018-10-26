4872e361836413624990579ef20113962cd821b7
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-26 14:03:50 +0530

    Update sub.py

--- sub.py
+++ sub.py
@@ -4,7 +4,7 @@ import numpy as np
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
-from sklearn.cross_validation import cross_val_score
+from sklearn.model_selection import cross_val_score
 
 def Model():
 	dataset =pd.read_csv('train.csv')
