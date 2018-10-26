083cb4a5830455d99cb773fce44d5bbbe4c3588d
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-26 21:43:25 +0530

    Update ML.py

--- ML.py
+++ ML.py
@@ -1,9 +1,10 @@
-          import numpy as np
-          import pandas as pd
+import numpy as np
+import pandas as pd
          
 
 
 def model_1():
+          
           data = pd.read_csv("finaldata.csv")
 
 
