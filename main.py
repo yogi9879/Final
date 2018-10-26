e9c2f7ea186e636b72edb1cc2b61b7a7f8888586
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-26 21:43:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def hello_world1():
         f = request.files['file']
         dataset =pd.read_csv(f)
         f.save(secure_filename(f.filename))
-        bot=ML.py()
+        bot=ML.model_1()
         
         return render_template("result.html",bot=bot)
 
