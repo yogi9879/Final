b2ea881023c8738ab289c706037331c52d72a2fb
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-26 11:27:04 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from flask import Flask,render_template, request
 app = Flask(__name__)
-import sub
-import numpy
+#import sub
+#import numpy
 
 
 
 @app.route('/')
 def hello_world():
   return  render_template("frontpage.html")
-
+'''
 @app.route('/hello', methods = ['GET', 'POST'])
 def hello_world1():
   f= = request.files['file']
   
   return  render_template("frontpage.html")
 
-
+'''
 
 if __name__ == '__main__':
   app.run()
