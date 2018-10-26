4ead891e5c3c6b617bdb17c51f56646dfc4db14d
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-26 21:49:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def hello_world1():
         f.save(secure_filename(f.filename))
         bot=ml1.model_1()
         
-        return render_template("result.html",bot=bot)
+        return render_template("result.html")
 
       
 
