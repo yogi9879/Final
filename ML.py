b6b48caebc21cecaaae53d88dd044f0661bfad93
yogi9879 <32189018+yogi9879@users.noreply.github.com>
2018-10-26 21:41:11 +0530

    Create ML.py

---
+++ ML.py
@@ -0,0 +1,36 @@
+          import numpy as np
+          import pandas as pd
+         
+
+
+def model_1(testdata):
+          data = pd.read_csv("finaldata.csv")
+
+
+          # Feature Scaling 
+          X_train = data.iloc[:,2:8] 
+          for i in range(len(X_train.columns)):
+             X_train.iloc[:,i]= X_train.iloc[:,i]/(np.max(X_train.iloc[:,i])-np.min(X_train.iloc[:,i])) - (np.mean(X_train.iloc[:,i])/(np.max(X_train.iloc[:,i])-np.min(X_train.iloc[:,i]))) 
+          X_train = X_train.fillna(0)
+
+          Y_train = data.iloc[:,-1]
+
+          from sklearn.linear_model import LinearRegression
+
+          model = LinearRegression()
+
+
+          lr=model.fit(X_train,Y_train)
+
+          predict = lr.predict(X_train)
+          pred = abs(predict)
+          predict1 = np.zeros(132)
+
+          for i in range(len(pred)):
+             predict1[i] = pred[i]/(1+pred[i])
+
+          cities = data.iloc[:,0]
+          return "hi"
+          
+
+          
